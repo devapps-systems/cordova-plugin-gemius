0489c70ee6b5275b57f0818ceef90b3f2941e141
Mahendra Liya <liyamahendra4@gmail.com>
2022-07-01 20:24:00 +0530

    initial plugin structure

---
+++ src/ios/GemiusPlugin.m
@@ -0,0 +1,28 @@
+/********* GemiusPlugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface GemiusPlugin : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation GemiusPlugin
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
