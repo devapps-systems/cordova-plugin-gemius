4a89ad528eb43ef0575dceb499ad8ddc6159e362
Mahendra Liya <liyamahendra4@gmail.com>
2022-07-02 00:46:29 +0530

    Added SDK initialization code and logEvent method

--- src/ios/GemiusPlugin.swift
+++ src/ios/GemiusPlugin.swift
@@ -1,5 +1,10 @@
 import UIKit
 import Foundation
+import GemiusSDK
+
+enum Season {
+  case FULL_PAGEVIEW, PARTIAL_PAGEVIEW, SONAR, ACTION, STREAM, DATA
+}
 
 @objc(GemiusPlugin) class GemiusPlugin : CDVPlugin {
     var _callbackId: String?
@@ -26,24 +31,55 @@ import Foundation
     }
     
     @objc private func didEnterBackground(notification: NSNotification){
-        initGemiusSDK()
+        GEMAudienceEvent.flushBuffer(withForce: true)
     }
-
+    
     func initGemiusSDK() {
         let gemiusConfig = Bundle.main.object(forInfoDictionaryKey: "GemiusPluginConfig") as! [String: String]
         let appName = gemiusConfig["gemius_app_name"]
         let appVersion = gemiusConfig["gemius_app_version"]
         let hitCollectorHost = gemiusConfig["gemius_hitcollector_host"]
         let scriptIdentifier = gemiusConfig["gemius_script_identifier"]
-        let loggingEnabled = gemiusConfig["gemius_logging_enabled"]
-        let idfaEnabled = gemiusConfig["gemius_idfa_enabled"]
-        let bufferedMode = gemiusConfig["gemius_buffered_mode"]
-        let powerSavingMode = gemiusConfig["gemius_power_saving_mode"]
         
+        let loggingEnabled = Bool(gemiusConfig["gemius_logging_enabled"]!)
+        let idfaEnabled = Bool(gemiusConfig["gemius_idfa_enabled"]!)
+        let bufferedMode = Bool(gemiusConfig["gemius_buffered_mode"]!)
+        let powerSavingMode = Bool(gemiusConfig["gemius_power_saving_mode"]!)
+     
+        GEMConfig.sharedInstance()?.setAppInfo(appName!, version: appVersion!)
+        GEMConfig.sharedInstance().loggingEnabled = loggingEnabled!
+        GEMConfig.sharedInstance().idfaCollectionEnabled = idfaEnabled!
+        
+        GEMAudienceConfig.sharedInstance().hitcollectorHost = hitCollectorHost!
+        GEMAudienceConfig.sharedInstance().scriptIdentifier = scriptIdentifier!
+        GEMAudienceConfig.sharedInstance().bufferedMode = bufferedMode!
+        GEMAudienceConfig.sharedInstance().powerSavingMode = powerSavingMode!
     }
 
     @objc(coolMethod:)
     func coolMethod(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+
+        let msg = command.arguments[0] as? String ?? ""
+        if msg.count > 0 {
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: msg
+            )
+        }
+
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
+    
+    @objc(logEvent:)
+    func logEvent(command: CDVInvokedUrlCommand) {
+        let firstString = command.arguments[0] as? String;
+        
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
--- www/GemiusPlugin.js
+++ www/GemiusPlugin.js
@@ -1,8 +1,19 @@
 var exec = require('cordova/exec');
 
 var GemiusPlugin = {
+    eventType: {
+        FULL_PAGEVIEW: 0,
+        PARTIAL_PAGEVIEW: 1,
+        SONAR: 2,
+        ACTION: 3,
+        STREAM: 4,
+        DATA: 5,
+    },
     coolMethod: function (arg0, success, error) {
         exec(success, error, 'GemiusPlugin', 'coolMethod', [arg0]);
+    },
+    logEvent: function(success, error, eventType, params) {
+        exec(success, error, 'GemiusPlugin', 'logEvent', [eventType, params]);
     }
 };
 
